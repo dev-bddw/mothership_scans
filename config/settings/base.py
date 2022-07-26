42f5a29e2a991f4781f8e7209871e0efefcbdcaa
Lance Knickerbocker <lance@bddw.com>
2022-07-26 11:30:30 -0400

    turn off account registration

--- config/settings/base.py
+++ config/settings/base.py
@@ -259,7 +259,7 @@ LOGGING = {
 
 # django-allauth
 # ------------------------------------------------------------------------------
-ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
+ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", False)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = "username"
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
