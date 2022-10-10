8796d901e6f1a4e54970ebabaec8c145cca416ba
lance <lance@bddw.com>
2022-10-10 16:28:22 -0400

    changed api url

--- v2/urls.py
+++ v2/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import all
+from .views import all, by_sku
 
 app_name = "v2"
 
-urlpatterns = [path("scans/", view=all, name="all")]
+urlpatterns = [
+    path("scans/", view=all, name="all"),
+    path("scans/<sku>/", view=by_sku, name="by-sku"),
+]
--- v2/views.py
+++ v2/views.py
@@ -7,13 +7,7 @@ from scans.models import Scan
 
 def all(request):
 
-    if request.GET.get("sku"):
-
-        q_set = Scan.objects.filter(sku=request.GET.get("sku"))
-
-    else:
-
-        q_set = Scan.objects.all()
+    q_set = Scan.objects.all()
 
     response = {
         "data": [
@@ -37,8 +31,39 @@ def all(request):
         response = {"data": None}
 
     json_response = JsonResponse(response, safe=False)
+
     json_response.headers["Access-Control-Allow-Origin"] = "*"
 
-    print(print(json_response.headers))
+    return json_response
+
+
+def by_sku(request, sku):
+
+    q_set = Scan.objects.filter(sku=sku)
+
+    response = {
+        "data": [
+            {
+                "type": "scans",
+                "id": s.scan_id if s.scan_id else "",
+                "attributes": {
+                    "sku": s.sku,
+                    "tracking": s.tracking,
+                    "time_scan": s.time_scan,
+                    "location": str(s.location),
+                    "time_upload": s.time_upload,
+                },
+            }
+            for s in q_set
+        ]
+    }
+
+    if len(response["data"]) < 1:
+
+        response = {"data": None}
+
+    json_response = JsonResponse(response, safe=False)
+
+    json_response.headers["Access-Control-Allow-Origin"] = "*"
 
     return json_response
