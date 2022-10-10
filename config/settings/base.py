4da1853ae7476aa3f2b1e7420043b92f5628c7ea
lance <lance@bddw.com>
2022-10-10 09:48:40 -0400

    whitelist

--- config/settings/base.py
+++ config/settings/base.py
@@ -344,13 +344,8 @@ JAZZMIN_UI_TWEAKS = {
     "actions_sticky_top": False,
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "https://www.bddwscans.com",
-    "https://bddwscans.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-]
-CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ["localhost"]
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
