0993ed33aba4529e06a930822271d4ba72255d71
lance <lance@bddw.com>
2022-10-10 11:51:17 -0400

    removed regex

--- config/settings/base.py
+++ config/settings/base.py
@@ -289,7 +289,7 @@ REST_FRAMEWORK = {
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
-CORS_URLS_REGEX = r"^/api/.*$"
+# CORS_URLS_REGEX = r"^/api/.*$"
 
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
