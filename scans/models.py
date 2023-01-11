c35032cd1326bea862feb6c09ecbda5c25bba9d1
lance <lance@bddw.com>
2023-01-11 13:56:47 -0500

    test

--- scans/models.py
+++ scans/models.py
@@ -85,6 +85,9 @@ class Fail(models.Model):
 
             self.scan.bin_success = True
             self.scan.save()
+            self.title = "RESOLVED"
+            self.details = "Success after resend"
+            self.details = response.status_code
             batch_id = random.randint(0, 10000)
             Success.objects.create(scan=self.scan, batch_id=batch_id)
             return True
